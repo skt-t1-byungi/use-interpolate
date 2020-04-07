384947ad7d64e6844dbad3042ec503ef0025ad8a
byungi <tiniwz@gmail.com>
2020-04-07 22:58:28 +0900

    fix: remove tag-name-parser

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-import { ReactNode, useMemo, isValidElement, createElement, cloneElement, Fragment } from 'react'
-import parser = require('tag-name-parser')
+import { cloneElement, createElement, Fragment, isValidElement, ReactNode, useMemo } from 'react'
 
-type TagNode = ReturnType<typeof parser>[number]
-type ReactElements = Record<string,ReactNode | ((children?: ReactNode) => ReactNode)>
+type TagNode = string | {name: string; single: true} | {name: string; single: false; children: TagNode[]}
+type ReactElements = Record<string, ReactNode | ((children?: ReactNode) => ReactNode)>
 type Parser = (text: string, opts?: ParserOptions) => TagNode[]
-interface ParserOptions {strict?: boolean, tag?: [string, string]}
+interface ParserOptions {strict?: boolean; tag?: [string, string]}
 
-const parserWithMemoHook = (text: string, opts: ParserOptions = {}) => useMemo(() => parser(text, opts), [text])
+const parserWithMemoHook = (text: string, opts: ParserOptions = {}) => useMemo(() => parse(text, opts), [text])
 
 const createInterpolateCreator = (parser: Parser) => (opts: ParserOptions = {}) => (text: string, elements: ReactElements) => {
     return ensureOneNode(nodesToReactNodes(parser(text, opts), elements))
 }
 
-export const createInterpolate = createInterpolateCreator(parser)
+export const createInterpolate = createInterpolateCreator(parse)
 
 export const createHook = createInterpolateCreator(parserWithMemoHook)
 
@@ -21,6 +20,59 @@ export const interpolate = createInterpolate()
 export const useInterpolate = createHook()
 
 export default useInterpolate
+
+function parse (str: string, opts: ParserOptions = {}) {
+    const strict = opts.strict ?? true
+    const tag = [opts.tag?.[0] ?? '<', opts.tag?.[1] ?? '>'] as const
+    const openTag = esc(tag[0])
+    const closeTag = esc(tag[1])
+
+    const re = new RegExp(openTag + '(\\/)?(\\w+)\\s?[^' + closeTag + '/]*(\\/)?' + closeTag, 'g')
+
+    let pos = 0
+    const root: {children: TagNode[]} = { children: [] }
+    let current: {children: TagNode[]; name?: string; single?: boolean} = root
+    const stacks = [root]
+    let matched
+
+    while (matched = re.exec(str)) {
+        const isClose = matched[1]
+        const tagName = matched[2]
+        const isSingle = matched[3]
+        const len = matched[0].length
+        const idx = matched.index
+
+        if (pos !== idx) current.children.push(str.slice(pos, idx))
+        pos = idx + len
+
+        if (isClose) {
+            if (current.name === tagName) {
+                current = stacks.pop()!
+            } else if (strict) {
+                throw new TypeError('[tag-name-parser] Invalid close tag. ("' + tag[0] + '/' + tagName + tag[1] + '":' + idx + ')')
+            }
+        } else {
+            if (isSingle) {
+                current.children.push({ name: tagName, single: true })
+            } else {
+                stacks.push(current)
+                current.children.push(current = { name: tagName, single: false, children: [] })
+            }
+        }
+    }
+
+    if (pos < str.length) current.children.push(str.slice(pos))
+
+    if (current !== root && strict) {
+        throw new TypeError('[tag-name-parser] Close tag ("' + tag[0] + current.name + tag[1] + '") is missing.')
+    }
+
+    return root.children
+}
+
+function esc (str: string) {
+    return str.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+}
 
 function ensureOneNode (nodes: ReactNode[]) {
     return nodes.length === 1 ? nodes[0] : createElement(Fragment, undefined, ...nodes)
